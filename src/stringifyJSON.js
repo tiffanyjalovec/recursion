092e12a6d858243e9339c3067fc6cf59db551fa4
Tiffany Jalovec <carr.tiffany@gmail.com>
2019-11-22 11:35:10 -0700

    stringify code

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,48 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
-};
+
+	if(typeof obj === "undefined"){
+		return "";
+	}
+
+	if(typeof obj === "function" || typeof obj === "undefined"){
+		return "";
+	}
+
+	if(obj === null){
+		return "null";
+	}
+
+	if (Array.isArray(obj)){
+     var stringArr = [];
+
+     obj.forEach(function(element){
+       stringArr.push(stringifyJSON(element));
+	    })
+	    return "[" + stringArr.toString() + "]";
+	  }
+
+     if (typeof obj === "object"){
+       var keys = Object.keys(obj);
+       var string = "";
+       keys.forEach(function(i){
+	    if(typeof i === "function" || typeof i === "undefined" || typeof obj[i] === "function" || typeof obj[i] === "undefined"){
+		  return "";
+	   }
+	    else {
+          string += stringifyJSON(i) + ':' + stringifyJSON(obj[i]) + ',';
+	  }
+    })
+      return "{" + string.slice(0, string.length -1) + "}"
+    }
+
+    if (typeof obj === "string"){
+    	return '"' + obj + '"';
+    }
+   return obj.toString();
+ };
+
+
+
+
